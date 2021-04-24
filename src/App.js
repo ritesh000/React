49ab5b56fd2a86389ff075344f24c6a20b79e9b7
r-malav <riteshserver@gmail.com>
2021-04-24 07:28:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Ritesh from './components/Ritesh'
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Ritesh/>
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Ritesh.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ritesh(){
+    return <h1>Hello Ritesh</h1>
+}
+
+export default ritesh
